7374a030fb5c38d1b1ce10ece29d0086bc08f9ad
lange <lange@remote.de>
2017-07-13 15:28:50 +0200

    Relocated module's configuration for proper patching

--- modules/ske/assets/patch/installed_modules.php
+++ modules/ske/assets/patch/installed_modules.php
@@ -1,14 +1,21 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 return array(
-    'encrypt'       => MODPATH . 'vendor/koseven/koseven/modules/encrypt',    // Encryption supprt
+    /**
+     *  Modules shipped with Koseven @see https://github.com/koseven/koseven
+     */
     'auth'          => MODPATH . 'vendor/koseven/koseven/modules/auth',       // Basic authentication
     'cache'         => MODPATH . 'vendor/koseven/koseven/modules/cache',      // Caching with multiple backends
     'codebench'     => MODPATH . 'vendor/koseven/koseven/modules/codebench',  // Benchmarking tool
     'database'      => MODPATH . 'vendor/koseven/koseven/modules/database',   // Database access
+    'encrypt'       => MODPATH . 'vendor/koseven/koseven/modules/encrypt',    // Encryption support
     'image'         => MODPATH . 'vendor/koseven/koseven/modules/image',      // Image manipulation
     'orm'           => MODPATH . 'vendor/koseven/koseven/modules/orm',        // Object Relationship Mapping
     'pagination'    => MODPATH . 'vendor/koseven/koseven/modules/pagination', // Pagination
     'unittest'      => MODPATH . 'vendor/koseven/koseven/modules/unittest',   // Unit testing
     'userguide'     => MODPATH . 'vendor/koseven/koseven/modules/userguide',  // User guide and API documentation
+
+    /**
+     *  Required SKE modules
+     */
 );
