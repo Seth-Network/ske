a7920b9a5a91e1a3d364ecc4a8853ec56dd0066d
Christoph Lange <christoph.lange@daimler.com>
2018-07-09 14:35:27 +0200

    Fixed paths with trailing slashes

--- modules/ske-core/assets/patch/index.php
+++ modules/ske-core/assets/patch/index.php
@@ -13,7 +13,7 @@ $application = 'application';
  *
  * @link http://kohanaframework.org/guide/about.install#modules
  */
-$modules = dirname(__FILE__). '/../';
+$modules = rtrim(dirname(__FILE__), '/'. DIRECTORY_SEPARATOR). '/../';
 
 /**
  * The directory in which the Kohana resources are located. The system
@@ -54,19 +54,19 @@ error_reporting(E_ALL | E_STRICT);
  */
 
 // Set the full path to the docroot
-define('DOCROOT', realpath(dirname(__FILE__). './../').DIRECTORY_SEPARATOR);
+define('DOCROOT', realpath(rtrim(dirname(__FILE__), '/'. DIRECTORY_SEPARATOR). '/..').DIRECTORY_SEPARATOR);
 
 // Make the application relative to the docroot, for symlink'd index.php
 if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
-	$application = DOCROOT.$application;
+    $application = DOCROOT.$application;
 
 // Make the modules relative to the docroot, for symlink'd index.php
 if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
-	$modules = DOCROOT.$modules;
+    $modules = DOCROOT.$modules;
 
 // Make the system relative to the docroot, for symlink'd index.php
 if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
-	$system = DOCROOT.$system;
+    $system = DOCROOT.$system;
 
 // Define the absolute paths for configured directories
 define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
@@ -78,8 +78,8 @@ unset($application, $modules, $system);
 
 if (file_exists('install'.EXT))
 {
-	// Load the installation check
-	return include 'install'.EXT;
+    // Load the installation check
+    return include 'install'.EXT;
 }
 
 /**
@@ -87,7 +87,7 @@ if (file_exists('install'.EXT))
  */
 if ( ! defined('KOHANA_START_TIME'))
 {
-	define('KOHANA_START_TIME', microtime(TRUE));
+    define('KOHANA_START_TIME', microtime(TRUE));
 }
 
 /**
@@ -95,7 +95,7 @@ if ( ! defined('KOHANA_START_TIME'))
  */
 if ( ! defined('KOHANA_START_MEMORY'))
 {
-	define('KOHANA_START_MEMORY', memory_get_usage());
+    define('KOHANA_START_MEMORY', memory_get_usage());
 }
 
 // Bootstrap the application
@@ -104,19 +104,19 @@ require APPPATH.'bootstrap'.EXT;
 
 if (PHP_SAPI == 'cli') // Try and load minion
 {
-	class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
-	set_exception_handler(array('Minion_Exception', 'handler'));
+    class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
+    set_exception_handler(array('Minion_Exception', 'handler'));
 
-	Minion_Task::factory(Minion_CLI::options())->execute();
+    Minion_Task::factory(Minion_CLI::options())->execute();
 }
 else
 {
-	/**
-	 * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
-	 * If no source is specified, the URI will be automatically detected.
-	 */
-	echo Request::factory(TRUE, array(), FALSE)
-		->execute()
-		->send_headers(TRUE)
-		->body();
+    /**
+     * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
+     * If no source is specified, the URI will be automatically detected.
+     */
+    echo Request::factory(TRUE, array(), FALSE)
+        ->execute()
+        ->send_headers(TRUE)
+        ->body();
 }
